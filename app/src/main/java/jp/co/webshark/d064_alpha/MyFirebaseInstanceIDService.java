560b6c4c0536c81a582ed25945d3599115ec2677
Takashi Yamamoto <>
2017-12-06 22:33:05 +0900

    トークンが取れた

--- app/src/main/java/jp/co/webshark/d064_alpha/MyFirebaseInstanceIDService.java
+++ app/src/main/java/jp/co/webshark/d064_alpha/MyFirebaseInstanceIDService.java
@@ -9,9 +9,6 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
-    public MyFirebaseInstanceIDService() {
-    }
-
     @Override
     public void onTokenRefresh() {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
